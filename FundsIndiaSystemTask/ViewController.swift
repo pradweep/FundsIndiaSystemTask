83e3e5f95bd12fa29860702b417cb3f503e9f705
pradeepiOS <pradeep.iostech@gmail.com>
2021-03-12 13:55:19 +0530

    Initial Commit

---
+++ FundsIndiaSystemTask/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FundsIndiaSystemTask
+//
+//  Created by Pradeep's Macbook on 12/03/21.
+//  Copyright © 2021 Motiv Ate Fitness. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
