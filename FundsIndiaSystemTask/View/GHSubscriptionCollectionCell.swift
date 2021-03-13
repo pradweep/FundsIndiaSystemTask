17ff2c2928151a2f23b0b95fcc837bccbe951fb2
pradeepiOS <pradeep.iostech@gmail.com>
2021-03-13 15:12:10 +0530

    Added subscription name label

--- FundsIndiaSystemTask/View/GHSubscriptionCollectionCell.swift
+++ FundsIndiaSystemTask/View/GHSubscriptionCollectionCell.swift
@@ -14,12 +14,19 @@ class GHSubscriptionCollectionCell: BaseCollectionCell {
     
     //MARK: - Views
     
-    lazy var avatarImageView: CachedImageView = {
+    private lazy var avatarImageView: CachedImageView = {
         let v = CachedImageView()
         v.backgroundColor = .clear
         v.contentMode = .scaleAspectFill
         v.layer.cornerRadius = 6
         v.layer.masksToBounds = true
+        return v
+    }()
+    
+    private lazy var subscriptionLabel: UILabel = {
+        let v = UILabel()
+        v.font = .systemFont(ofSize: 17, weight: .medium)
+        v.textAlignment = .center
         return v
     }()
     
@@ -30,14 +37,17 @@ class GHSubscriptionCollectionCell: BaseCollectionCell {
     }
     
     fileprivate func setupConstraints() {
-        self.addSubview(avatarImageView)
-        avatarImageView.fillSuperview()
+        self.addSubviewsToParent(avatarImageView,
+                                 subscriptionLabel)
+        avatarImageView.anchor(top: self.topAnchor, leading: self.leadingAnchor, bottom: self.bottomAnchor, trailing: self.trailingAnchor, padding: .init(top: 0, left: 0, bottom: 25, right: 0), size: .init())
+        subscriptionLabel.anchor(top: avatarImageView.bottomAnchor, leading: self.leadingAnchor, bottom: self.bottomAnchor, trailing: self.trailingAnchor, padding: .init(top: 4, left: 0, bottom: 0, right: 0), size: .init(width: 0, height: 21))
     }
     
     func setData(with item: GHSubscriptionModel) {
         if let strAvatar = item.owner?.avatar_url {
             self.avatarImageView.loadImage(urlString: strAvatar)
         }
+        self.subscriptionLabel.text = item.name?.capitalizingFirstLetter()
     }
     
     //MARK: - Helper Functions
